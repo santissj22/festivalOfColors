f35913aa2a98c7a9bcc20695f58cbe326a59a5d4
Santiago Mondragon <coolschoolguy2332@gmail.com>
2020-04-07 22:44:20 -0700

    working on section d

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,3 +7,22 @@ function initMap() {
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
   }
+
+
+
+// Drop down menu
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
